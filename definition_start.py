2cba4b7d289a10856cb2f9dfaed32d0d89067446
Wendy Chen <lfwendy@gmail.com>
2023-10-18 02:52:34 +0800

    Create definition_start.py

---
+++ definition_start.py
@@ -0,0 +1,22 @@
+# Python Obejct Oriented Programming by Joe Marini course - practice
+# Basic class definitions
+
+# TODO: create a basic class
+class Book:
+    def __init__(self, title):
+      self.title = title
+      
+# TODO: create instances of the class
+book1 = Book("Working Backwards")
+book2 = Book("Bet on Yourself")
+book3 = Book("The Little Prince")
+      
+# TODO: print the lcass and property
+print(book1)
+print(book1.title)
+
+print(book2)
+print(book2.title)
+
+print(book3)
+print(book3.title)
